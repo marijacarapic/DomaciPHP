99727ad6b08e8bbb84dc445a37f6d1ebf9c8b606
Marija Carapic <mc20190116@student.fon.bg.ac.rs>
2023-09-03 19:56:50 +0200

    Dodavanje zanra knjige

---
+++ model/zanr.php
@@ -0,0 +1,11 @@
+<?php
+class Zanr{
+  public $zanrId;
+  public $nazivZanra;
+  function __construct($zanrId=null,$nazivZanra=null) {
+        $this->zanrId = $zanrId;
+        $this->nazivZanra = $nazivZanra;
+    }
+   
+}
+?>
